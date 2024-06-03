0308ee1ffc3c0d8f49943b9da2d6b801c744dcba
Akhil Raj <akhilraaaj7@gmail.com>
2024-06-03 12:59:08 +0530

    added video modal with tooltip and done minor style fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,54 @@ function closeNavbar() {
     sideBar.classList.remove('opacity-100','h-full');
 }
 
+// Hero Play Button Modal
+var openmodal = document.querySelectorAll('.modal-open');
+for (var i = 0; i < openmodal.length; i++) {
+  openmodal[i].addEventListener('click', function(event){
+    event.preventDefault();
+    toggleModal();
+  });
+}
+
+const overlay = document.querySelector('.modal-overlay');
+overlay.addEventListener('click', toggleModal);
+
+var closemodal = document.querySelectorAll('.modal-close');
+for (var i = 0; i < closemodal.length; i++) {
+  closemodal[i].addEventListener('click', toggleModal);
+}
+
+document.onkeydown = function(evt) {
+  evt = evt || window.event;
+  var isEscape = false;
+  if ("key" in evt) {
+    isEscape = (evt.key === "Escape" || evt.key === "Esc");
+  } else {
+    isEscape = (evt.keyCode === 27);
+  }
+  if (isEscape && document.body.classList.contains('modal-active')) {
+    toggleModal();
+  }
+};
+
+function toggleModal() {
+  const body = document.querySelector('body');
+  const modal = document.querySelector('.modal');
+  const iframe = document.querySelector('#modal-video');
+  
+  modal.classList.toggle('opacity-0');
+  modal.classList.toggle('pointer-events-none');
+  body.classList.toggle('modal-active');
+
+  if (modal.classList.contains('opacity-0')) {
+    // Remove the src attribute to stop the video
+    iframe.removeAttribute('src');
+  } else {
+    // Re-add the src attribute to start the video
+    iframe.setAttribute('src', 'https://www.youtube.com/embed/j90-QRILido');
+  }
+}
+
 // hero page 
 // if(document.getElementById('mv-left-arrow')){
 //     toggleHeroChildMovement();
@@ -51,8 +99,6 @@ function closeNavbar() {
         scheduleNextToggle();
     }
 })();
-
-
 
 // Custom Button
 const button = document.querySelector('.custom-btn');
